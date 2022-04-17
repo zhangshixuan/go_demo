17a5aa49cbac6f2eb124ef4c305cb868263a266c
zhangshixuan <zhangshixuan925@163.com>
2022-04-17 21:15:54 +0800

    1。闭包

---
+++ src/closure/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 闭包演示
+	// 闭包 = 类变量， 匿名函数 = 类方法
+	upper := AddUpper()
+	fmt.Println(upper(1))
+	fmt.Println(upper(2))
+}
+
+// 累加器
+func AddUpper() func(int) int {
+	n := 10
+	return func(x int) int {
+		n += x
+		return n
+	}
+}
