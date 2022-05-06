b5e1225ec9085c8d6b1352702059ecfd01602207
zhangshixuan <zhangshixuan925@163.com>
2022-05-06 22:11:27 +0800

    1。接口

---
+++ src/interface/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+//定义一个usb接口
+type Usb interface {
+	Start()
+	Stop()
+}
+
+//定义一个相机结构体
+type Camera struct {
+}
+
+//让相机结构体方法，实现usb接口的开始方法
+func (c Camera) Start() {
+	fmt.Println("相机开始工作")
+}
+
+//让相机结构体方法，实现usb接口的结束方法
+func (c Camera) Stop() {
+	fmt.Println("相机结束工作")
+}
+
+type Phone struct {
+}
+
+func (p Phone) Start() {
+	fmt.Println("手机开始工作")
+}
+
+func (p Phone) Stop() {
+	fmt.Println("手机结束工作")
+}
+
+// 计算机结构体
+type Computer struct {
+}
+
+//编写一个方法Working方法，接收一个Usb接口类型变量
+//只要是实现了Usb接口（所谓实现Usb接口，就是指实现了Usb接口声明所有方法）
+func (c Computer) Working(usb Usb) {
+	usb.Start()
+	usb.Stop()
+}
+
+/**
+接口
+*/
+func main() {
+	camera := Camera{}
+	phone := Phone{}
+	computer := Computer{}
+	computer.Working(camera)
+	computer.Working(phone)
+}
