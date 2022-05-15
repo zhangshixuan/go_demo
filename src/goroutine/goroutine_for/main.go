afc377748bf94579a3050dca48689f4434ad8c92
zhangshixuan <zhangshixuan925@163.com>
2022-05-15 09:00:39 +0800

    协程for循环试验

--- src/goroutine/goroutine_for/main.go
+++ src/goroutine/goroutine_for/main.go
@@ -8,10 +8,19 @@ import "fmt"
 */
 func main() {
 	boolChan := make(chan bool, 3)
-	boolChan <- true
-	boolChan <- false
-	boolChan <- true
-	close(boolChan)
+
+	go func() {
+		boolChan <- true
+		boolChan <- false
+		boolChan <- true
+		// 只有关闭管道，才能出现!ok跳出的状态
+		close(boolChan)
+	}()
+
+	//for i := 0; i < 3; i++ {
+	//	b := <-boolChan
+	//	fmt.Println(b)
+	//}
 
 	for true {
 		b, ok := <-boolChan
